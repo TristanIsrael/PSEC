57b69158704af3ce540130fdd382cd7eb7e44974
Tristan Israël <5672131+tibsys@users.noreply.github.com>
2024-11-20 23:22:00 +0100

    Améliorations

---
+++ setup/packages/psec/psec-core/generate-x-config.py
@@ -0,0 +1,95 @@
+import json, subprocess
+
+def angle_to_rotate(angle):
+    mapping = {
+        0: "normal",
+        90: "left",
+        -90: "right",
+        180: "inverted"
+    }
+    return mapping.get(angle, "normal")
+
+'''
+def compute_resolution(resolution, angle):
+    # Extraire la largeur et la hauteur de la chaîne de résolution
+    largeur, hauteur = map(int, resolution.split('x'))
+    
+    # Calculer la nouvelle résolution en fonction de l'angle
+    if angle == 90 or angle == -270:
+        # Rotation de 90° ou -270° : échange largeur et hauteur
+        nouvelle_resolution = f"{hauteur}x{largeur}"
+    elif angle == 180 or angle == -180:
+        # Rotation de 180° : la résolution reste la même
+        nouvelle_resolution = f"{largeur}x{hauteur}"
+    elif angle == 270 or angle == -90:
+        # Rotation de 270° ou -90° : échange largeur et hauteur
+        nouvelle_resolution = f"{hauteur}x{largeur}"
+    else:
+        # Pour les autres angles (0° ou multiples de 360°), la résolution reste la même
+        nouvelle_resolution = f"{largeur}x{hauteur}"
+    
+    return nouvelle_resolution
+'''
+
+def write_monitor_section(angle:int) -> None:
+    filepath = "/etc/X11/xorg.conf.d/10-monitor.conf"    
+
+    # Lecture de la résolution normale
+    with open('/sys/class/graphics/fb0/virtual_size', 'r') as fichier:
+        resolution = fichier.read().strip().replace(",", "x")
+        #print(resolution)
+
+    # Calcul de la résolution
+    if resolution is None:
+        resolution = "1024x768"
+
+    #new_resolution = compute_resolution(resolution, angle)
+    #print(new_resolution)
+
+    # Commande pour appliquer la rotation
+    section = """
+Section "Monitor"
+    Identifier "Monitor0"
+    Option "PreferredMode" "{}"
+    Option "Rotate" "{}" 
+    Option "DPMS" "false"
+EndSection
+""".format(resolution, angle_to_rotate(angle))
+            
+    with open(filepath, "w") as fichier:
+        fichier.write(section)
+
+if __name__ == "__main__":
+    print("Generate X server configuration files")
+
+    # Rotation is defined in topology.json
+    topology_file="/etc/psec/topology.json"
+
+    try:
+        with open(topology_file, 'r') as file:
+            json_data = json.load(file)
+    except Exception as e:
+        print("An error occured while reading the topology file {}".format(topology_file))    
+        print(e)    
+        exit(1)    
+
+    #"gui": {
+    #    "use": 1,    
+    #    "screen": {
+    #        "rotation": 0
+    json_gui = json_data.get("gui")
+    if json_gui is None:
+        print("No GUI in topology")
+        exit(0)
+        
+    json_use = json_gui.get("use")
+    if json_use is None:
+        print("GUI is unset in topology")
+        exit(0)
+
+    json_screen = json_gui.get("screen")
+    if json_screen is not None:        
+        rotation = json_screen.get("rotation")
+               
+        write_monitor_section(rotation)
+    
