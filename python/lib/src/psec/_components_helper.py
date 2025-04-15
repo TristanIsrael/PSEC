017402a60dd84ced3aa1e27a4a4a3c6c63245d11
Tristan Israël <5672131+tibsys@users.noreply.github.com>
2025-04-15 22:21:38 +0200

    Update _components_helper.py

--- python/lib/src/psec/_components_helper.py
+++ python/lib/src/psec/_components_helper.py
@@ -2,13 +2,11 @@ from . import EtatComposant
 
 class ComponentsHelper():
 
-    components_ = []
-
     def __init__(self):
-        components_ = []
+        self.__components = []
 
     def update(self, updates:list) -> None:
-        components_dict = {comp["id"]: comp for comp in self.components_}
+        components_dict = {comp["id"]: comp for comp in self.__components}
     
         for update in updates:
             if update["id"] in components_dict:
@@ -16,29 +14,30 @@ class ComponentsHelper():
                 components_dict[update["id"]]["state"] = update["state"]
             else:
                 # Si l'id n'existe pas, ajouter l'entrée
-                self.components_.append(update)
+                self.__components.append(update)
 
     def get_by_id(self, id:str) -> dict:
-        for comp in self.components_:
+        for comp in self.__components:
             if comp.get("id") is not None and comp.get("id") == id:
                 return comp
+            
         return {}
 
     def get_ids(self) -> list[str]:
-        return [d["id"] for d in self.components_ if "id" in d]
+        return [d["id"] for d in self.__components if "id" in d]
     
     def get_states(self) -> dict:
-        return {comp["id"]: comp["state"] for comp in self.components_ if "id" in comp and "state" in comp}
+        return {comp["id"]: comp["state"] for comp in self.__components if "id" in comp and "state" in comp}
     
     def get_state(self, id:str):
-        for comp in self.components_:
+        for comp in self.__components:
             if comp.get("id") == id:
                 return comp.get("state", EtatComposant.UNKNOWN)
             
         return EtatComposant.UNKNOWN
     
     def get_type(self, id:str) -> str:
-        for comp in self.components_:
+        for comp in self.__components:
             if comp.get("id") == id:
                 return comp.get("type", "")
             
@@ -47,8 +46,11 @@ class ComponentsHelper():
     def get_ids_by_type(self, type:str) -> list[str]:
         ids = list()
 
-        for comp in self.components_:
+        for comp in self.__components:
             if comp.get("type") is not None and comp.get("type") == type:
                 ids.append(comp.get("id", ""))
             
         return ids
+    
+    def get_components(self):
+        return self.__components
