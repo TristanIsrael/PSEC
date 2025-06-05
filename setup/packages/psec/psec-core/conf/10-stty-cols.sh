9eb0c4bc5e8895089f3e13d82cde734f81ddbfe3
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2025-06-05 23:37:10 +0200

    Improve debug console

---
+++ setup/packages/psec/psec-core/conf/10-stty-cols.sh
@@ -0,0 +1,7 @@
+# In case we connect from the serial debug port
+# We need to set the columns to a larger value
+# because by default it is 80.
+
+if [ "$TERM" = "vt100" ]; then 
+    stty cols 200
+fi
