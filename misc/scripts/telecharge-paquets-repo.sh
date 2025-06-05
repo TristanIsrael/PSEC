5043e72b66f507a9dd93bcdcefa480b0a0832e5d
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2025-06-06 00:17:52 +0200

    Update telecharge-paquets-repo.sh
    Moved to rsync instead of http, which is easier and more compatible for syncing mirrors

--- misc/scripts/telecharge-paquets-repo.sh
+++ misc/scripts/telecharge-paquets-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WGET_OPTS='-c -N -q -m -p -E -k -K -np -l 1 -nd -e robots=off --show-progress --reject=html,htm'
+#WGET_OPTS='-c -N -q -m -p -E -k -K -np -l 1 -nd -e robots=off --show-progress --reject=html,htm'
 
 if [ "$#" -ne 2 ]; then
     echo "Arguments manquant :"
@@ -8,9 +8,11 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+MIRROR_UNC="mirrors.ircam.fr"
+MIRROR_ROOT="pub"
 
-REPO_MAIN_ROOT="http://dl-cdn.alpinelinux.org/alpine/v$2/main"
-REPO_COMMUNITY_ROOT="http://dl-cdn.alpinelinux.org/alpine/v$2/community"
+REPO_MAIN_ROOT="rsync://$MIRROR_UNC/$MIRROR_ROOT/alpine/v$2/main"
+REPO_COMMUNITY_ROOT="rsync://$MIRROR_UNC/$MIRROR_ROOT/alpine/v$2/community"
 
 REPO_MAIN_URL="$REPO_MAIN_ROOT/$1"
 REPO_COMMUNITY_URL="$REPO_COMMUNITY_ROOT/$1"
@@ -19,10 +21,12 @@ echo "Téléchargement des paquets Alpine v$2 pour l'architecture $1"
 
 mkdir -p alpine/v$2/community/$1
 echo "URL $REPO_COMMUNITY_URL"
-wget $WGET_OPTS -P alpine/v$2/community/$1 $REPO_COMMUNITY_URL 
+rsync -avz --delete $REPO_COMMUNITY_URL/ alpine/v$2/community/$1/
+#wget $WGET_OPTS -P alpine/v$2/community/$1 $REPO_COMMUNITY_URL
 
 mkdir -p alpine/v$2/main/$1
 echo "URL $REPO_MAIN_URL"
-wget $WGET_OPTS -P alpine/v$2/main/$1 $REPO_MAIN_URL
+rsync -avz --delete $REPO_MAIN_URL/ alpine/v$2/main/$1/
+#wget $WGET_OPTS -P alpine/v$2/main/$1 $REPO_MAIN_URL
 
 echo "Téléchargement terminé"
