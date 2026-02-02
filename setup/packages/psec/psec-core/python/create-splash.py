a5a642a83a03a457d0a2f56941859fe21d8d1dfb
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-02 12:45:11 +0100

    #83

--- setup/packages/psec/psec-core/python/create-splash.py
+++ setup/packages/psec/psec-core/python/create-splash.py
@@ -1,13 +1,39 @@
 from PIL import Image
 import sys
 import os
+import json
 
 LOGO_FILEPATH = "/boot/splash.png"
+TOPOLOGY_FILE="/etc/psec/topology.json"
+DEFAULT_BGCOLOR = "#1ca9f7"
+
+def get_bgcolor():
+    try:
+        with open(TOPOLOGY_FILE, 'r') as file:
+            json_data = json.load(file)
+
+            json_product = json_data.get("product")
+            
+            if json_product is None:
+                print("No product definition in topology")
+                return DEFAULT_BGCOLOR
+
+            # Read the backgroung color value
+            json_bgcolor = json_product.get("splash_bgcolor")
+            if json_bgcolor is None:
+                print("There is no splash_bgcolor value in the topology. Using default")
+                return DEFAULT_BGCOLOR
+            
+            return json_bgcolor
+    except Exception as e:
+        print("An error occured while reading the topology file {}".format(TOPOLOGY_FILE))    
+        print(e)
+        return DEFAULT_BGCOLOR
 
 def create_splash(width:int, height:int) -> Image:
     print(f"Create splash of size {width}x{height}")
 
-    splash = Image.new("RGB", (width, height), "#1ca9f7")
+    splash = Image.new("RGB", (width, height), get_bgcolor())
     image = Image.open(LOGO_FILEPATH)
     
     image_width, image_height = image.size
