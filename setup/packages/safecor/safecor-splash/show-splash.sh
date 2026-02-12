0093c482875b558dd0c72686c13112f07f76f509
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-12 15:19:31 +0100

    #95

--- setup/packages/safecor/safecor-splash/show-splash.sh
+++ setup/packages/safecor/safecor-splash/show-splash.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if grep -qw "nosplash" /proc/cmdline; then
-    ewarn "Splash explicitely disabled on kernel command line"
-    return 0
+    echo "Splash explicitely disabled on kernel command line"
+    exit 0
 fi
 
 nohup fbi -d /dev/fb0 -T 1 -noverbose -nointeractive /boot/splash.png  > /dev/null 2>&1 &
