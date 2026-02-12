c7545513636b59e251cde9005fb8cd995862990e
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-02-12 12:01:31 +0100

    #95

--- setup/packages/safecor/safecor-splash/show-splash.sh
+++ setup/packages/safecor/safecor-splash/show-splash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nohup fbi -d /dev/fb0 -T 1 -noverbose -nointeractive /boot/splash_ng_nobg.png  > /dev/null 2>&1 &
+nohup fbi -d /dev/fb0 -T 1 -noverbose -nointeractive /boot/splash.png  > /dev/null 2>&1 &
